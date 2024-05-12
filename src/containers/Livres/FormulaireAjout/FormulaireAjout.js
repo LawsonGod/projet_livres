8b33ef0e636282392caadece282db418b390d724
LawsonGod <godwill.lawson@gmail.com>
2024-05-12 20:07:34 +0200

    Ajout des Inputs dans leformulaire et gestion de son state

--- src/containers/Livres/FormulaireAjout/FormulaireAjout.js
+++ src/containers/Livres/FormulaireAjout/FormulaireAjout.js
@@ -1,10 +1,52 @@
 import React, {Component} from 'react';
+import Buttons from '../../../components/Buttons/Button';
 
 class FormulaireAjout extends Component {
+    state = {
+        titreSaisi: "",
+        auteurSaisi: "",
+        nombreDePageSaisi: ""
+    }
+
     render(){
         return (
             <>
                 <h2 className='text-center text-primary' style = {{fontFamily:'Sigmar One'}}>Affichage du formulaire d'ajout</h2>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="titre" className="form-label">Titre du livre</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="titre" 
+                            value={this.state.titreSaisi}
+                            onChange={(event) => this.setState({titreSaisi:event.target.value})}  
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="auteur" className="form-label">Auteur</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="auteur" 
+                            value={this.state.auteurSaisi}
+                            onChange={(event) => this.setState({auteurSaisi:event.target.value})}  
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="nombreDePage" className="form-label">Nombre de pages</label>
+                        <input 
+                            type="number" 
+                            className="form-control" 
+                            id="nombreDePage"
+                            value={this.state.nombreDePageSaisi}
+                            onChange={(event) => this.setState({nombreDePageSaisi:event.target.value})}
+                        />
+                    </div>
+                    <Buttons typeBtn="btn-primary">Valider</Buttons>
+                </form>
             </>
         );
     }
